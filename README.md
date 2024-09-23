8be3070e574ba981648b173694cd0a309476d2ff
Kaushik Jain <kaushik.jain@intimetec.com>
2024-09-23 09:32:13 +0530

    ep-7 completed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-mongodb+srv://NamasteNode:HcM1uzZSFggAQMtY@namastenode.l8mtm.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode
+## Lec-6 : DB, Schemas and Models | Mongoose
+
+Cluster > Database > Collection > Document > Field
+
+NamasteNode > devTinder > User >
+
+---
+
+URL of cluster :
+
+```jsx
+mongodb+srv://NamasteNode:HcM1uzZSFggAQMtY@namastenode.l8mtm.mongodb.net/
+```
+
+In code when connecting use /Database name as well like :
+
+```jsx
+mongodb+srv://NamasteNode:HcM1uzZSFggAQMtY@namastenode.l8mtm.mongodb.net/devTinder/devTinder
+```
+
+---
+
+## Lec-7 : Diving into API’s
--- devTinder/src/App.js
+++ devTinder/src/App.js
@@ -3,23 +3,60 @@ const { adminAuth, userAuth } = require("./middleware/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  // {
+  //   "fName": "Mohit",
+  //   "lName": "Pamnani",
+  //   "emailId": "kaushik@gmail.com",
+  //   "password": "kaushik#2@1",
+  //   "gender": "Female"
+  // }
 
-app.post("/signup", async (_, res) => {
-  const userObj = {
-    fName: "Kaushik",
-    lName: "jain",
-    age: 23,
-    emailId: "kaushik@gmail.com",
-    password: "kaushik#2@1",
-    gender: "Female",
-  };
-
-  const user = new User(userObj);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User signup success");
   } catch (err) {
     res.status(400).send("Error saving user", err);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await User.find({});
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("Error ", err);
+  }
+});
+app.get("/user", async (req, res) => {
+  try {
+    const data = await User.find({ emailId: req.body.emailId });
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("Error", err);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const data = await User.findOneAndUpdate(
+      { emailId: req.body.emailId },
+      req.body
+    );
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("Error", err);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const data = await User.findOneAndDelete({ emailId: req.body.emailId });
+    res.send(data);
+  } catch (err) {
+    res.status(400).send("Error", err);
   }
 });
 
